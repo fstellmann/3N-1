c2a1a8f7714ecb96dad75ddd0cf8ef1f9aa73854
Florian Stellmann <f.stellmann@nasdo.de>
2022-06-11 14:13:11 +0200

    Initial Commit

---
+++ 3N+1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Numerics;
+
+namespace _3N_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BigInteger erg = BigInteger.Parse("1");
+            BigInteger hold = erg;
+
+            while(true)
+            {
+                erg = hold;
+                Console.Write($"Processing {erg}");
+                int stepCount = 0;
+                while(erg != 1)
+                {
+                    erg = erg % 2 == 0 ? erg / 2 : erg * 3 + 1;
+                    stepCount++;
+                }
+                Console.WriteLine($"  --> Took {stepCount} steps.");
+                hold++;
+            }
+        }
+    }
+}
